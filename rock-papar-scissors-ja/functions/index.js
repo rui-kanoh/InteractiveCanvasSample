67d6b8a17b158658f874df3079b391c4b05a9eb8
rui.kanoh <rui.kanoh@gmail.com>
2019-08-29 20:59:14 +0900

    AddTestApp

---
+++ rock-papar-scissors-ja/functions/index.js
@@ -0,0 +1,86 @@
+const functions = require('firebase-functions');
+const {
+    dialogflow,
+    HtmlResponse
+  } = require('actions-on-google');
+  
+  const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+  
+  const app = dialogflow({
+    debug: true
+  });
+  
+  app.intent('Default Welcome Intent', conv => {
+    conv.ask('どの手を出しますか？グー？チョキ？それともパー？');
+    conv.ask(new HtmlResponse({
+      url: `https://${firebaseConfig.projectId}.firebaseapp.com/`
+    }));
+  });
+
+  const pronoun = {
+    rock: 'グー',
+    scissors: 'チョキ',
+    paper: 'パー'
+  };
+  
+  const judgeMap = {
+    rock: {
+      rock: 'あいこです。',
+      paper: 'あなたの負けです。',
+      scissors: 'あなたの勝ちです！'
+    },
+    paper: {
+      rock: 'あなたの勝ちです！',
+      paper: 'あいこです。',
+      scissors: 'あなたの負けです。'
+    },
+    scissors: {
+      rock: 'あなたの負けです。',
+      paper: 'あなたの勝ちです！',
+      scissors: 'あいこです。'
+    }
+  };
+
+  app.intent('Show', (conv, param) => {
+    // ユーザが出した手を取得する。
+    const userChoice = param['user-choice'].toLowerCase();
+    // アクションの手をランダムに決定する。
+    const actionChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+    // 勝敗を示すメッセージを取得する。
+    const message = judgeMap[userChoice][actionChoice];
+    // SSMLを使って返事を組み立てる。
+    const ssml = `
+      <speak>
+        <p>はい、私も何を出すか決めました。</p>
+        <p>では、じゃんけんぽん！</p>
+        <p>あなたは、${pronoun[userChoice]}を出しました。</p>
+        <p>私は、${pronoun[actionChoice]}を出しました。</p>
+        <p>${message}</p>
+        <break time="400ms" />
+        <p>もう一度遊びますか？</p>
+      </speak>`;
+    conv.ask(ssml);
+    // 画面を更新するための情報を持つHtmlResponseオブジェクト。
+    conv.ask(new HtmlResponse({
+      data: {
+        scene: 'result',
+        userChoice,
+        actionChoice,
+        message
+      }
+    }));
+    
+    app.intent('Show - yes', conv => {
+        conv.ask('わかりました。どの手を出しますか？グー？チョキ？それともパー？');
+        conv.ask(new HtmlResponse({
+          data: {
+            scene: 'restart'
+          }
+        }));
+      });
+  });
+
+
+  
+  exports.fulfillment = functions.https.onRequest(app);
+  
---
+++ rock-papar-scissors-ja/public/js/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+document.querySelectorAll('#welcome img').forEach(img => {
+    img.addEventListener('click', elem => {
+      interactiveCanvas.sendTextQuery(elem.target.dataset.choice);
+    });
+  });
+
+interactiveCanvas.ready({
+    onUpdate(data) {
+      // Display the versus image.
+      if (data.scene === 'result') {
+        document.querySelector('#welcome').style.display = 'none';
+        document.querySelector('#vs').style.display = 'block';
+        document.querySelector('#user-choice').src = `images/${data.userChoice}.png`;
+        document.querySelector('#action-choice').src = `images/${data.actionChoice}.png`;
+        document.querySelector('#message').innerText = data.message;
+        // Display the result.
+        setTimeout(() => {
+            document.querySelector('#vs').style.display = 'none';
+            document.querySelector('#result').style.display = 'block';
+            document.querySelector('#message').style.display = 'block';
+          }, 5000);
+  
+      }
+      // Initialize the screen.
+      if (data.scene === 'restart') {
+        document.querySelector('#welcome').style.display = 'block';
+        document.querySelector('#vs').style.display = 'none';
+        document.querySelector('#result').style.display = 'none';
+        document.querySelector('#message').style.display = 'none';
+      }
+  
+    }
+  });
